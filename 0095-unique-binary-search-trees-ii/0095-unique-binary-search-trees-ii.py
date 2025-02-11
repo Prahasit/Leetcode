bd58cee73dc479b3aee32b4856d5732355da3203
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-02-11 10:04:56 -0800

    Time: 7 ms (17.31%), Space: 18.8 MB (54.85%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -7,20 +7,22 @@
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
         
-        dp = [[[] for _ in range(n + 1)] for _ in range(n + 1)]
-        for i in range(1, n + 1):
-            dp[i][i] = [TreeNode(i)]
-
-        for numberOfNodes in range(2, n + 1):
-            for start in range(1, n - numberOfNodes + 2):
-                end = start + numberOfNodes - 1
-                for i in range(start, end + 1):
-                    left_subtrees = dp[start][i - 1] if i != start else [None]
-                    right_subtrees = dp[i + 1][end] if i != end else [None]
-
-                    for left in left_subtrees:
-                        for right in right_subtrees:
-                            root = TreeNode(i, left, right)
-                            dp[start][end].append(root)
-
-        return dp[1][n]
+        def generate_trees(start, end):
+            if start > end:
+                return [None,]
+            
+            all_trees = []
+            for i in range(start, end + 1):
+                left_trees = generate_trees(start, i - 1)
+                right_trees = generate_trees(i + 1, end)
+                
+                for l in left_trees:
+                    for r in right_trees:
+                        current_tree = TreeNode(i)
+                        current_tree.left = l
+                        current_tree.right = r
+                        all_trees.append(current_tree)
+            
+            return all_trees
+        
+        return generate_trees(1, n) if n else []
