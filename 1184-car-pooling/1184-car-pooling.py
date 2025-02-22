1ffd7a80f51cee4c4fc57a17b45a688cc17c4bbf
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-02-22 15:09:56 -0800

    Time: 3 ms (67.49%), Space: 18.3 MB (32.14%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.py
+++ 1184-car-pooling/1184-car-pooling.py
@@ -1,15 +1,18 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        hopOnOff = []
-        for count, start, end in trips:
-            hopOnOff.append((start, count))
-            hopOnOff.append((end, -count))
+        heap = []
+        trips.sort(key = lambda x: x[1])
 
-        hopOnOff.sort()
+        for cap, start, end in trips:
+            while heap and heap[0][0] <= start: #if end time of top of heap is less than the start time of new value
+                capacity += heap[0][1]
+                heapq.heappop(heap)
 
-        inCar = 0
-        for destination, passangers in hopOnOff:
-            inCar += passangers
-            if inCar > capacity:
+            if cap > capacity:
                 return False
+
+            else:
+                capacity -= cap
+                heapq.heappush(heap, (end, cap)) #end time, capacity
+
         return True
