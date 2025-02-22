c469aa8a567ceb234ee159dee467b0270cca86e4
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-02-22 10:12:42 -0800

    Time: 0 ms (100%), Space: 17.8 MB (63.47%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -2,8 +2,12 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         res = []
         freq = Counter(words)
-        #to sort in descending order based on frequency and if equal sort in ascending order based on key
-        sorted_order = sorted(freq.items(), key = lambda x : (-x[1], x[0]))
-        for key, val in sorted_order[:k]: #extracting only k elements
-            res.append(key)
+        pq = [(-count,word) for word, count in freq.items()]
+        heapq.heapify(pq)
+        res = []
+
+        while k > 0:
+            count, word = heapq.heappop(pq)
+            res.append(word)
+            k -= 1
         return res
