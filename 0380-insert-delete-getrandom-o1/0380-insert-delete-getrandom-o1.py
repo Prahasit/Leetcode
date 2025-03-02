d2a1dd046e1dc0a514aa30bd4f47da7137041289
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-02 13:05:39 -0800

    Time: 120 ms (73.47%), Space: 58.2 MB (11.67%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,18 +1,23 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.random = set()
+        self.dict = {}
+        self.list = []
 
     def insert(self, val: int) -> bool:
-        if val not in self.random:
-            self.random.add(val)
+        if val not in self.dict:
+            self.dict[val] = len(self.list)
+            self.list.append(val)
             return True
         else:
             return False
 
     def remove(self, val: int) -> bool:
-        if val in self.random:
-            self.random.remove(val)
+        if val in self.dict:
+            last_element, idx = self.list[-1], self.dict[val]
+            self.list[idx], self.dict[last_element] = last_element, idx #swap with last element
+            self.list.pop()
+            del self.dict[val]
             return True
         else:
             return False
@@ -20,7 +25,7 @@ class RandomizedSet:
 
     def getRandom(self) -> int:
         #set is unorderded, we cannot to indexing so converting to a tuple.
-        return random.choice(tuple(self.random))
+        return random.choice(self.list)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
