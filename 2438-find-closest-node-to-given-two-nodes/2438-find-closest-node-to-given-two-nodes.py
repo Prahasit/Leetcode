575276056726caa2295b3b15dde47b4f52f9b239
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-03 12:43:01 -0800

    Time: 197 ms (41.03%), Space: 70.2 MB (5.02%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.py
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.py
@@ -1,33 +1,32 @@
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
 
-        def bfs(node, dist):
-            q = deque()
-            q.append(node)
-            visit = [False] * n
-            dist[node] = 0
-            while q:
-                node = q.popleft()
-                if visit[node]:
-                    continue
-                visit[node] = True
-                nei = edges[node]
-                if nei != - 1 and not visit[nei]:
-                    dist[nei] = dist[node] + 1
-                    q.append(nei)
+        def dfs(node, dist, visit):
+            visit[node] = True
+            nei = edges[node]
+            if nei != -1 and not visit[nei]:
+                dist[nei] = dist[node] + 1
+                dfs(nei, dist, visit)
+                     
 
         n = len(edges)
         dist1 = [-1] * n
         dist2 = [-1] * n
+        visit1, visit2 = [False] * n, [False] * n
 
-        bfs(node1, dist1)
-        bfs(node2, dist2)
+        #initialize  the initialize nodesd distance to 0
+        dist1[node1] = 0
+        dist2[node2] = 0
+
+        dfs(node1, dist1, visit1)
+        dfs(node2, dist2, visit2)
         closest_node =  - 1
         min_dist = float('inf')
 
         for i in range(n):
-            if min(dist1[i], dist2[i]) >= 0 and max(dist1[i], dist2[i]) < min_dist:
-                min_dist = max(dist1[i], dist2[i])
-                closest_node = i
+            if dist1[i] != -1 and dist2[i] != -1:
+                if min(dist1[i], dist2[i]) >= 0 and max(dist1[i], dist2[i]) < min_dist:
+                    min_dist = max(dist1[i], dist2[i])
+                    closest_node = i
         
         return closest_node
