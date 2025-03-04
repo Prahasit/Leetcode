d89a706f9a1befff8681bdb9b6d757cad960267e
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-03 22:07:06 -0800

    Time: 44 ms (19.26%), Space: 18.1 MB (81.11%) - LeetHub

--- 1676-minimum-number-of-days-to-eat-n-oranges/1676-minimum-number-of-days-to-eat-n-oranges.py
+++ 1676-minimum-number-of-days-to-eat-n-oranges/1676-minimum-number-of-days-to-eat-n-oranges.py
@@ -1,22 +1,31 @@
 class Solution:
     def minDays(self, n: int) -> int:
-        # for ech step 1 orange less is fine
-        #other options include if multiple of 2 or multiple of 3 then.
+        memo = {}
+        q = deque()
+        q.append(n)
+        days = 0
+        while q:
+            temp = deque()
+            for _ in range(len(q)):
+                orange = q.popleft()
+
+                if orange == 0:
+                    return days
+                    
+                if orange not in memo:
+                    memo[orange] = days
+
+                    temp.append(orange - 1)
+                    if orange % 2 == 0:
+                        temp.append(orange // 2)
+                    if orange % 3 == 0:
+                        temp.append(orange // 3)
+            q = temp
+            days += 1
         
-        def dfs(num):
-    
-            if num <= 1:
-                return num
-            if num in dp:
-                return dp[num]
-            
-            option2 = dfs(num// 2) + num % 2
-            option3 = dfs(num//3) + num % 3
+        return days
 
-            dp[num] =  1 + min(option2, option3)
-
-            return dp[num]
                 
-        dp = defaultdict(int)
-        return dfs(n)
+
+
         
