fb8fa40cd9bcec5812ff427cc940aad4ba711edb
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-05 15:15:53 -0800

    Time: 4 ms (57.15%), Space: 17.8 MB (82.56%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
@@ -1,22 +1,19 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         last_day = days[-1]
-        dp = [-1] * (last_day + 1)
+        dp = [0] * (last_day + 1)
         travel_days = set(days) # converted to set for easy lookup i.e O(1) lookups
-        def solve(curr_day):
-            if curr_day > days[-1]:
-                return 0
-            if dp[curr_day] != - 1:
-                return dp[curr_day]
-            if curr_day not in  travel_days: #just skip
-                dp[curr_day] =  solve(curr_day + 1)
-                return dp[curr_day]
+        for curr_day in range(1, last_day + 1):
+
+            if curr_day not in travel_days: #just skip
+                dp[curr_day] =  dp[curr_day - 1]
                 
-            one_day = costs[0] + solve(curr_day + 1)
-            seven_days = costs[1] + solve(curr_day + 7)
-            thirty_days = costs[2] + solve(curr_day + 30)
+            else:
+                one_day = costs[0] + dp[curr_day - 1]
+                seven_days = costs[1] + dp[max(0,curr_day - 7)]
+                thirty_days = costs[2] + dp[max(0,curr_day - 30)]
 
-            dp[curr_day] = min(one_day, seven_days, thirty_days)
-            return dp[curr_day]
+                dp[curr_day] = min(one_day, seven_days, thirty_days)
+            
 
-        return solve(1)
+        return dp[last_day]
