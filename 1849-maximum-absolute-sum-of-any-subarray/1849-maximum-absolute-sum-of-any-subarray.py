92eb4105966bc422ae16f5facbd4d4953d7f6330
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-05 16:15:36 -0800

    Time: 44 ms (82.78%), Space: 28.5 MB (60.98%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
@@ -1,23 +1,14 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
-        # cannot be solved with take, not take as its subarray not subsequence
-        #max_sum is max +Ve sum and max -ve sum
-        n = len(nums)
-        res_max, res_min = 0, 0
-        total_max_sum = 0
-        total_min_sum = 0
-        result = 0
-        for i in range(n):
-            total_max_sum += nums[i]
-            res_max = max(res_max, total_max_sum)
-            if total_max_sum < 0:
-                total_max_sum = 0
-        
-        
-            total_min_sum += nums[i]
-            res_min = min(res_min, total_min_sum)
-            if total_min_sum > 0:
-                total_min_sum = 0
+        min_prefix = 0
+        max_prefix = 0
+        prefix_sum = 0
 
-        result = max(res_max, abs(res_min))
-        return result
+        for num in nums:
+            prefix_sum += num
+
+            min_prefix = min(min_prefix, prefix_sum)
+            max_prefix = max(max_prefix, prefix_sum)
+
+        return max_prefix - min_prefix
+        
