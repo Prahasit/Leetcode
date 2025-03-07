d1d3d89f67bd9cdbd7b579843ee468d787061bdc
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-06 18:35:00 -0800

    Time: 24 ms (74.83%), Space: 18.2 MB (93.07%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -1,18 +1,23 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        # same at finding  min among the path and find at last max(found, grid[m - 1][n - 1])
         m, n = len(grid), len(grid[0])
-        heap = [(grid[0][0], 0, 0)]
+        directions = [[0,1], [0, -1], [1, 0], [-1, 0]]
         visit = set()
-        directions = [[0, 1], [0 , -1], [1, 0], [-1, 0]]
+        heap = [(grid[0][0], 0, 0)]
+        time = 0
         while heap:
-            elevation, r, c = heapq.heappop(heap)
+            dist, r, c = heapq.heappop(heap)
             if r == m - 1 and c == n - 1:
-                return elevation
-            
+                return dist
+            visit.add((r,c))
+            time += 1
             for dr, dc in directions:
                 row, col = dr + r, dc + c
-                if 0 <= row < m and 0 <= col < n and (row,col) not in visit:
-                    visit.add((row, col))
-                    heapq.heappush(heap, (max(elevation, grid[row][col]), row, col))
+                if 0 <= row < m and 0 <= col < n  and (row, col) not in visit:
+                    visit.add((row,col))
+                    time = max(dist, grid[row][col])
+                    heapq.heappush(heap, (time, row, col))
         
+        
+
+    
