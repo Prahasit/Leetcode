af9e9aca6ac2f4d6c0f333fb9bc7f779fa71fd8e
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-10 11:04:00 -0700

    Time: 0 ms (100%), Space: 17.7 MB (97.96%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -3,19 +3,14 @@ class Solution:
         #we find a way that 'leet', 'code' in leetcode--> 'leet' is there
         # it is better time complexity instead of fintinf each index in leetcode to  wordDict
 
-        def solve(i):
-            if i == len(s):
-                return True
-            if i in memo:
-                return memo[i]
-            
-            for w in wordDict:
-                if i + len(w) <= len(s) and s[i: i + len(w)] == w:
-                    if solve(i + len(w)):
-                        memo[i] = True
-                        return memo[i]
-            memo[i] = False
-            return memo[i]
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
 
-        memo = {}
-        return solve(0)
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+
+        return dp[0]
