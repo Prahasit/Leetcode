6a5c90857398f138e123171cd12caf369bb604e8
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-14 16:29:31 -0700

    Time: 9009 ms (5.01%), Space: 237.3 MB (5.08%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,20 +1,17 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        #we are taking prev_ind as - 1 but we cant store index as - 1 in array so we do coordinate change where - 1 will be 0, 0 will be 1 etc so adding + 1
+        #hence len of dp is dp[n][n + 1]
         n = len(nums)
-        def solve(ind, prev_val):
+        dp = [[-1 for _ in range(n + 1)] for _ in range(n)]
+        def dfs(ind, prev_ind):
             if ind == n:
                 return 0
-            if (ind, prev_val) in memo:
-                return memo[(ind, prev_val)]
-
-            not_take = solve(ind + 1, prev_val)
-
-            take = float('-inf')
-            if nums[ind] > prev_val:
-                take = 1 + solve(ind + 1, nums[ind])
-            
-            memo[(ind, prev_val)] = max(not_take, take)
-            return memo[(ind, prev_val)]
-
-        memo = {}
-        return solve(0, float('-inf'))
+            if dp[ind][prev_ind] != -1:
+                return dp[ind][prev_ind]
+            len = 0 + dfs(ind + 1, prev_ind) #not take
+            if prev_ind == -1 or nums[ind] > nums[prev_ind]:
+                len = max( len, 1 + dfs(ind + 1, ind)) # max of take and not take
+            dp[ind][prev_ind] = len
+            return dp[ind][prev_ind]
+        return dfs(0, - 1)
