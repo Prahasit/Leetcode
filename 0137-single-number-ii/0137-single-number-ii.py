673752a2f06007282c3943f49ae9117d2459249c
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-17 16:37:04 -0700

    Time: 0 ms (100%), Space: 19.8 MB (8.2%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,6 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq = Counter(nums)
+        for key,val in freq.items():
+            if val == 1:
+                return key
