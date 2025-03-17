4e487d67ea3b91ddc15b3597acb7a526cc3bf51a
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-17 12:01:30 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,23 +1,26 @@
+
+
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        min_heap = []  # min-heap to store and retrieve the smallest ugly number
-        seen_numbers = set()  # set to avoid duplicates
-        prime_factors = [2, 3, 5]  # factors for generating new ugly numbers
-
-        heapq.heappush(min_heap, 1)
-        seen_numbers.add(1)
-
-        current_ugly = 1
-        for _ in range(n):
-            current_ugly = heapq.heappop(min_heap)  # get the smallest number
-
-            # generate and push the next ugly numbers
-            for prime in prime_factors:
-                next_ugly = current_ugly * prime
-                if next_ugly not in seen_numbers:  # avoid duplicates
-                    heapq.heappush(min_heap, next_ugly)
-                    seen_numbers.add(next_ugly)
-
-        return current_ugly
+        
+        heap = [1]
+        heapq.heapify(heap)
+        res = []
+        res_set = set()
+        while len(res) < n:
+            num = heapq.heappop(heap)
+            if num not in res_set:
+                res_set.add(num)
+                res.append(num)
+            
+            if num * 2 not in res_set:
+                heapq.heappush(heap, num * 2)
+            if num * 2 not in res_set:
+                heapq.heappush(heap, num * 3)
+            if num * 2 not in res_set:
+                heapq.heappush(heap, num * 5) 
+            
+        
+        return max(res)
 
         
