59fa0ab697440a65a665661250b88bcacbaf98df
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-25 20:10:28 -0700

    Time: 28 ms (74.61%), Space: 21.4 MB (36.16%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,26 +1,13 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        #directly finding == goal is difficult as if goal comes, there will be ambiguity wherther to update r or l as if we update one some cases will be missed. # fo example check for example 1 you will get to know
-        # So what we can do is find the algorith for <= goal and then find for <= goal - 1 and subtract them i.e goal - (goal - 1) will will the answer
+        #if sum - k in hashmap then we are sure there is a subarray with sum k 
+        cumulative_sum = defaultdict(int) # sum, no of occurences of corresponding sum
+        tot_sum, count = 0, 0
+        cumulative_sum[0] = 1
 
-        def subarraysum(goal):
-            l, r = 0, 0
-            count = 0
-            cur_sum = 0
-            if goal < 0:
-                return 0
-            while r < n:
-                cur_sum += nums[r]
-                while cur_sum > goal:
-                    cur_sum -= nums[l]
-                    l += 1
-                count += (r - l + 1)
-
-                r += 1
-            return count
-
-
-        n = len(nums)
-        return subarraysum(goal) - subarraysum(goal - 1)
-
-            
+        for num in nums:
+            tot_sum += num
+            if tot_sum - goal in cumulative_sum:
+                count += cumulative_sum[tot_sum - goal]
+            cumulative_sum[tot_sum] += 1
+        return count
