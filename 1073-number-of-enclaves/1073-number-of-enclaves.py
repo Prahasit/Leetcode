586af1b73ce2f55b861aa5f44184c620628bb673
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-27 16:28:46 -0700

    Time: 123 ms (35.64%), Space: 37.5 MB (25.78%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -1,32 +1,31 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        total = 0
-        res = 0
-        visit = set()
+        
 
-        def solve(r, c):
+        def dfs(r, c):
             if r < 0 or c < 0 or r >= m or c >= n or grid[r][c] == 0:
+                return 0 
+            if (r, c) in visit:
                 return 0
-            if (r,c) in visit:
-                return 0
-            
-            visit.add((r,c))
+            visit.add((r, c))
+
             length = 1
-            length += solve(r - 1, c)
-            length += solve(r + 1, c)
-            length += solve(r, c - 1)
-            length += solve(r, c + 1)
+            length += dfs(r - 1, c)
+            length += dfs(r + 1, c)
+            length += dfs(r, c - 1)
+            length += dfs(r, c + 1)
 
             return length
 
-
+        m, n = len(grid), len(grid[0])
+        visit = set()
+        res = 0
+        total = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     total += 1
-                
-                    if (i ,j) not in visit and i in [0, m - 1] or j in [0, n - 1]:
-                        res += solve(i, j)
-
+                    if (i, j) not in visit and i in [0, m - 1] or j in [0, n - 1]:
+                        res += dfs(i, j)
+        
         return total - res
