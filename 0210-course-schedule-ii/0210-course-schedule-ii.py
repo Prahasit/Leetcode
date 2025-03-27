f937f6ddee7b1151d7882d3896f53112f376b0ab
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-27 11:13:48 -0700

    Time: 5 ms (38.43%), Space: 19 MB (73.13%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -2,12 +2,13 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         adj_list = defaultdict(list)
         indegree = [0] * numCourses
-        q = deque()
-        visit = set()
-        res = []
         for src, dst in prerequisites:
             adj_list[dst].append(src)
             indegree[src] += 1
+
+        q = deque()
+        visit = set()
+        res = []
 
         for i in range(numCourses):
             if indegree[i] == 0:
@@ -15,12 +16,11 @@ class Solution:
 
         while q:
             node = q.popleft()
-            res.append(node)
             visit.add(node)
+            res.append(node)
             for nei in adj_list[node]:
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
                     q.append(nei)
-        
+
         return res if len(visit) == numCourses else []
-        
