fed6afaa53c7583865ae84746fba0870fbbb5ad1
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-27 14:56:26 -0700

    Time: 303 ms (15.56%), Space: 29.1 MB (5.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,37 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        
-        def dfs(r,c):
-            if r < 0 or c < 0 or r >= m or c >= n or grid[r][c] == '0':
-                return 
-            if (r, c) in visit:
-                return 
-            visit.add((r,c))
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        visited = set()
 
-            dfs(r - 1,c)
-            dfs(r + 1,c)
-            dfs(r,c - 1)
-            dfs(r,c + 1)
-
+        def explore(row, col):
+            row_inbounds = 0 <= row < len(grid)
+            col_inbounds = 0 <= col < len(grid[0])
+            if not row_inbounds or not col_inbounds:
+                return False
             
+            if grid[row][col] == "0":
+                return False
 
+            pos = (row, col)
+            if pos in visited:
+                return False
+            visited.add(pos)
 
-        m,n = len(grid), len(grid[0])
-        visit = set()
-        res = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1' and (i, j) not in visit:
-                    dfs(i ,j)
-                    res+= 1
-        return res
+            explore(row - 1, col)
+            explore(row + 1, col)
+            explore(row, col - 1)
+            explore(row, col + 1)
+
+            return True
+
+        count = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if explore(row, col):
+                    count += 1
+        return count
+
+        
