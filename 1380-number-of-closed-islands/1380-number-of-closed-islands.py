e36e28f24472c2f4ac7441471a957e96d4c4f502
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-27 16:49:34 -0700

    Time: 23 ms (39.08%), Space: 19.8 MB (11.16%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.py
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.py
@@ -1,29 +1,34 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        visit = set()
-        def solve(r, c):
-            if r < 0 or c < 0 or r >=m or c >= n:
-                return -1
-            if (r, c) in visit or grid[r][c] == 1:
-                return True
+        
 
-            visit.add((r, c))
-            
-            
-            up = solve(r - 1, c)
-            down = solve(r + 1, c) 
-            left = solve(r, c - 1) 
-            right = solve(r, c + 1) 
-
-            if up == - 1 or down == - 1 or left == - 1 or right == - 1:
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= m or c >= n:
                 return - 1
-            return True
+            if (r,c) in visit or grid[r][c] == 1:
+                return True
+            
+            visit.add((r,c))
 
+            up = dfs(r - 1, c)
+            down = dfs(r + 1, c)
+            left = dfs(r, c - 1)
+            right = dfs(r, c + 1)
+
+            if up == - 1 or down == - 1 or left == - 1 or right == -1:
+                return - 1
+            
+            return True
+                
+        m, n = len(grid), len(grid[0])
         res = 0
+        visit = set()
         for i in range(m):
             for j in range(n):
-                if (i, j) not in visit and grid[i][j] == 0:
-                    if solve(i, j) != -1:
+                if (i, j ) not in visit and grid[i][j] == 0:
+                    if dfs(i, j) != -1:
                         res += 1
+
         return res
+
+                    
