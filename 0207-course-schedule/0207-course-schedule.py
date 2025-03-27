2b19fb2d71fd9c915fc9772ebfff4a0522084e2c
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-27 10:29:15 -0700

    Time: 4 ms (69.83%), Space: 18.8 MB (98.52%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,16 +2,16 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj_list = defaultdict(list)
         indegree = [0] * numCourses
-        q = deque()
-        visit = set()
         for src, dst in prerequisites:
             adj_list[dst].append(src)
             indegree[src] += 1
 
+        visit = set()
+        q = deque()
         for i in range(numCourses):
             if indegree[i] == 0:
                 q.append(i)
-
+        
         while q:
             node = q.popleft()
             visit.add(node)
@@ -19,7 +19,5 @@ class Solution:
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
                     q.append(nei)
-        
-        return numCourses == len(visit)
 
-        
+        return numCourses == len(visit)
