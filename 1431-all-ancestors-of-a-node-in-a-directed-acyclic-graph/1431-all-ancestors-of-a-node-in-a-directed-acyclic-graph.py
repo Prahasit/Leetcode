a6b9e5966b0164f8c2205fb55f34266a144a19d3
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-29 12:30:43 -0700

    Time: 255 ms (33.22%), Space: 34.7 MB (75.36%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,30 +1,30 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        #just reverse the edges and find the edges whic are reachable
+        # reversing the edges and finding reachability
 
-        #reversing the nodes
         adj_list = defaultdict(list)
+        result = []
         for src, dst in edges:
             adj_list[dst].append(src)
+
         
         def dfs(node):
-            visit.add(node)
+            
+
             for nei in adj_list[node]:
                 if nei not in visit:
+                    visit.add(nei)
                     dfs(nei)
 
-        answer = []
+
         for i in range(n):
+            visit = set()
             l = []
-            visit = set() # all visited nodes are here and so can add it to the result      
             dfs(i)
-            for node in range(n):
-                if node == i:
-                    continue
-                if node in visit:
-                    l.append(node)
-            answer.append(l)
+            for node in visit:
+                l.append(node)
+            l.sort()
+            result.append(l)
 
-        return answer
-
-        
+        return result
+            
