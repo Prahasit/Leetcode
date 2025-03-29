d6b65e4e793921757a7c553ad4654ebf7b4bffcb
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-29 13:27:06 -0700

    Time: 593 ms (25.5%), Space: 20.4 MB (60.3%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.py
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -1,28 +1,27 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        result = []
+        
         adj_list = defaultdict(list)
         for src, dst in prerequisites:
             adj_list[src].append(dst)
         
-        def dfs(source, target):
-            if source == target:
+        def dfs(node, destination):
+            if node == destination:
                 return True
-            visit[source] = True
-            for nei in adj_list[source]:
-                if not visit[nei]:
-                    if dfs(nei, target):
-                        return True
 
+            visit[node] = True
+            for nei in adj_list[node]:
+                if not visit[nei]:
+                    if dfs(nei, destination):
+                        return True
             return False
 
-
+        result = []
         for q in queries:
             visit = [False] * numCourses
-            if dfs(q[0], q[1]): #finding dfs with the range of query
+            if dfs(q[0], q[1]):
                 result.append(True)
             else:
                 result.append(False)
-        return result
 
-        
+        return result
