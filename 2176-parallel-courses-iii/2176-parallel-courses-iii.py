1fec54ed77bc3e0bcbe012316bd09a149c044e21
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-29 14:46:40 -0700

    Time: 163 ms (40.73%), Space: 76.1 MB (7.93%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.py
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.py
@@ -1,28 +1,25 @@
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-
         adj_list = defaultdict(list)
-        indegree = [0] * n
-        max_time = [0] * n
         for src, dst in relations:
             adj_list[src - 1].append(dst - 1)
-            indegree[dst - 1] += 1
-        
-        q = deque()
-        for node in range(n):
-            if indegree[node] == 0:
-                q.append(node)
-                max_time[node] = time[node]
-        while q:
-            node = q.popleft()
+
+        @lru_cache(None)
+        def dfs(node):
+            temp = 0
             for nei in adj_list[node]:
-                max_time[nei] = max(max_time[nei], max_time[node] + time[nei])
+                temp = max(temp, dfs(nei))
 
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    q.append(nei)
+            if not adj_list[node]:
+                return time[node]
+            return temp + time[node]
+        res = 0
+        for i in range(n):
+            res = max(res, dfs(i))
 
-        return max(max_time)
+        return res
+        
+        
 
 
         
