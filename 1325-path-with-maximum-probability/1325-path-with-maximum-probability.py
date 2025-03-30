a1c00cd5ffa0f11500ce231a77d6a31b287229fc
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-30 08:10:14 -0700

    Time: 127 ms (35.14%), Space: 28.7 MB (60.99%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -5,20 +5,20 @@ class Solution:
             adj_list[src].append((dst, succProb[i]))
             adj_list[dst].append((src, succProb[i]))
 
-        prob = [0.0] * n
-        prob[start_node] = 1
-        q = deque()
-        q.append((start_node, prob[start_node])) # node, probability
+        max_heap = [(-1, start_node)] # -1 as its minheap so -1 * 1 = -1
+        visit = set()
+        while max_heap:
+            prob, node = heapq.heappop(max_heap)
+            visit.add(node)
 
-        while q:
-            node, cur_prob = q.popleft()
-            
-            for nei, p in adj_list[node]:
-                if cur_prob * p  > prob[nei]:
-                    prob[nei] = cur_prob * p
-                    q.append((nei, prob[nei]))
+            if node == end_node:
+                return -prob
 
-        return prob[end_node]
+            for nei, edge_prob in adj_list[node]:
+                if nei not in visit:
+                    heapq.heappush(max_heap, (prob * edge_prob, nei))
+        return 0
+
             
 
 
