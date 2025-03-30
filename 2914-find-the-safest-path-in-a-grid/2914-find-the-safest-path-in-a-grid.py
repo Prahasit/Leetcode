b42d8e64092a28928c1961b3db48314491a2b414
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-30 09:48:30 -0700

    Time: 3885 ms (64.33%), Space: 74.9 MB (7.42%) - LeetHub

--- 2914-find-the-safest-path-in-a-grid/2914-find-the-safest-path-in-a-grid.py
+++ 2914-find-the-safest-path-in-a-grid/2914-find-the-safest-path-in-a-grid.py
@@ -1,45 +1,49 @@
 class Solution:
     def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
-        #1. we add the '1' in queue
-        #2. find the min distance of each '0' from '1'
-        #3. now apply dijkstra's algorithm
-        n = len(grid)
-        directions = [[0, 1], [0, - 1], [1, 0], [-1, 0]]
+        # apply bfs to get the distances of '0's from '1's.
+        #then apply dikstra usng max_heap to traverse the maximum paths
 
         def bfs():
             q = deque()
             min_dist = {}
-            for r in range(n):
-                for c in range(n):
-                    if grid[r][c] == 1:
-                        q.append((r, c, 0)) # adding '1'  in queue with dist = 0
-                        min_dist[(r, c)] = 0
-
+            for i in range(n):
+                for j in range(n):
+                    if grid[i][j] == 1:
+                        q.append((i,j , 0))
+                        min_dist[(i, j)] = 0
+            
             while q:
                 r, c, dist = q.popleft()
                 for dr, dc in directions:
                     row, col = dr + r, dc + c
-                    if 0 <= row < n and 0 <= col < n and (row, col) not in min_dist:
+                    if 0 <= row < n and 0<= col < n and (row, col) not in min_dist:
                         min_dist[(row, col)] = dist + 1
                         q.append((row, col, dist + 1))
+
             return min_dist
 
+        n = len(grid)
+        directions = [[0,1], [0,-1], [1,0], [-1, 0]]
         min_dist = bfs()
+        
 
-        max_heap = [(-min_dist[(0,0)],0, 0)]
+        heap = [(-min_dist[(0,0)], 0, 0)]
         visit = set()
-        visit.add((0 , 0))
+        visit.add((0, 0))
 
-        while max_heap:
-            dist, r, c = heapq.heappop(max_heap)
+        while heap:
+            dist, r, c = heapq.heappop(heap)
             dist = -dist
             if r == n - 1 and c == n - 1:
                 return dist
             
             for dr, dc in directions:
-                row, col = r + dr, c + dc
-                if 0 <= row < n and 0 <= col < n and (row, col) not in visit:
-                    visit.add((row, col))
-                    distance = min(dist, min_dist[(row, col)]) # as we need minimum manhattan distance
-                    heapq.heappush(max_heap, (-distance, row, col)) # we need to push max -ve number 
-            
+                row, col = dr + r, dc + c
+                if 0 <= row < n and 0 <= col < n:
+                    if (row, col) not in visit:
+                        visit.add((row, col))
+                        distance = min(dist, min_dist[(row,col)])
+                        heapq.heappush(heap, (-distance, row, col))
+
+        
+        return 0
