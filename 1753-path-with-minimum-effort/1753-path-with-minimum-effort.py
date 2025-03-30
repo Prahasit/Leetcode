c656cb8e02ef6b286613d73c16bc44a90508e949
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-29 17:49:22 -0700

    Time: 269 ms (90.09%), Space: 19.5 MB (65.03%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.py
@@ -1,23 +1,26 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         m, n = len(heights), len(heights[0])
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        pq = []
-        heapq.heappush(pq,(0, 0, 0)) #dist, row, col
+       
+        directions = [[0, 1], [0, -1], [1, 0], [-1,0]]
         dist = [[float('inf')] * n for _ in range(m)]
-        dist[0][0] = 0
-        abs_min = float('-inf')
-        while pq:
-            diff, row, col, = heapq.heappop(pq)
-            if row == m - 1 and col == n - 1:
-                return diff
-            for dr, dc in directions:
-                r, c = row + dr, col + dc
-                if 0 <= r < m and 0 <= c < n:
-                    new_effort = max(abs(heights[row][col]- heights[r][c]), diff)
-                    if new_effort < dist[r][c]:
-                        dist[r][c] = new_effort
-                        heapq.heappush(pq, (new_effort, r, c))
+        heap = []
+        heapq.heappush(heap, (0 , 0 , 0))
 
+        dist[0][0] = 0
+        while heap:
+            diff, r, c = heapq.heappop(heap)
+            if r == m - 1 and c == n - 1:
+                return diff
+
+            for dr, dc in directions:
+                row, col = dr + r, dc + c
+                if 0 <= row < m and 0 <= col < n:
+                    new_effort = max(abs(heights[row][col] - heights[r][c]),diff)
+                    if new_effort < dist[row][col]:
+                        dist[row][col] = new_effort
+                        heapq.heappush(heap, (new_effort, row, col))
 
         return 0
+            
+
