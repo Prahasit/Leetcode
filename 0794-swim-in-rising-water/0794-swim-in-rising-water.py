334a29213d84823bad8a11c6b2162de5da8e80a7
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-30 20:38:57 -0700

    Time: 23 ms (85.88%), Space: 18.5 MB (59.16%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -1,23 +1,18 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        directions = [[0,1], [0, -1], [1, 0], [-1, 0]]
+        n = len(grid)
+        heap = []
+        heapq.heappush(heap, (grid[0][0], 0 , 0))
         visit = set()
-        heap = [(grid[0][0], 0, 0)]
-        time = 0
+        directions = [[0,1], [0, -1], [ 1, 0], [-1, 0]]
+
         while heap:
-            dist, r, c = heapq.heappop(heap)
-            if r == m - 1 and c == n - 1:
-                return dist
-            visit.add((r,c))
-            time += 1
+            elevation, r, c = heapq.heappop(heap)
+            if r == n - 1 and c == n - 1:
+                return elevation
+
             for dr, dc in directions:
                 row, col = dr + r, dc + c
-                if 0 <= row < m and 0 <= col < n  and (row, col) not in visit:
-                    visit.add((row,col))
-                    time = max(dist, grid[row][col])
-                    heapq.heappush(heap, (time, row, col))
-        
-        
-
-    
+                if 0 <= row < n and 0 <= col < n and (row, col) not in visit:
+                    visit.add((row, col))
+                    heapq.heappush(heap, (max(elevation, grid[row][col]), row, col))
