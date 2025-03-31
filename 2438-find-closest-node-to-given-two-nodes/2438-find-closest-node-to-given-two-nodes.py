bd4b84991c4c01bbb1f92c00322ff0db154e7098
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-03-30 20:06:27 -0700

    Time: 189 ms (44.67%), Space: 70.2 MB (6.8%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.py
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.py
@@ -1,32 +1,33 @@
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
 
-        def dfs(node, dist, visit):
+        def dfs(visit, node, dist):
             visit[node] = True
             nei = edges[node]
             if nei != -1 and not visit[nei]:
                 dist[nei] = dist[node] + 1
-                dfs(nei, dist, visit)
-                     
+                dfs(visit, nei, dist)
 
         n = len(edges)
         dist1 = [-1] * n
         dist2 = [-1] * n
-        visit1, visit2 = [False] * n, [False] * n
-
-        #initialize  the initialize nodesd distance to 0
         dist1[node1] = 0
         dist2[node2] = 0
 
-        dfs(node1, dist1, visit1)
-        dfs(node2, dist2, visit2)
-        closest_node =  - 1
+        visit1 = [False] * n
+        visit2 = [False] * n
+
+        dfs(visit1, node1, dist1)
+        dfs(visit2, node2, dist2)
+
         min_dist = float('inf')
+        closest = -1
 
         for i in range(n):
             if dist1[i] != -1 and dist2[i] != -1:
                 if min(dist1[i], dist2[i]) >= 0 and max(dist1[i], dist2[i]) < min_dist:
                     min_dist = max(dist1[i], dist2[i])
-                    closest_node = i
+                    closest = i
         
-        return closest_node
+
+        return closest
