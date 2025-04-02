247fc7288218a8b0e81585c0b2da19ab54b90406
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-04-02 16:08:12 -0700

    Time: 4 ms (70.59%), Space: 17.9 MB (36.76%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
@@ -1,13 +1,17 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        # O(n^2) --> need to maximize the k and i only
+        #finding leftmax and rightmax for every element
         n = len(nums)
         res = 0
+        left_max = [0] * n
+        right_max = [0] * n
+
+        for i in range(1, n):
+            left_max[i] = max(left_max[i - 1], nums[i - 1])
+            right_max[n - i - 1] = max(right_max[n - i], nums[n - i])
+        print(left_max)
+        for j in range(1, n - 1):
+            res = max(res, (left_max[j] - nums[j]) * right_max[j])
+
         
-        for k in range(2, n):
-            max_prefix = nums[0]
-            for j in range(1, k):
-                res = max(res, (max_prefix - nums[j]) * nums[k])
-                max_prefix = max(max_prefix, nums[j])
         return res
-                
