c5eded2ea176ed248c99b6f7586bd60fbc272ee8
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-04-02 11:21:28 -0700

    Time: 0 ms (100%), Space: 17.9 MB (23.55%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.py
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.py
@@ -6,16 +6,17 @@
 #         self.right = right
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
-        #first val is root, until the elements in array are less than(<) root val all of them are on left sub tree otherwise all are on right sub tree.
-
+        #preorder is root first
         if not preorder:
             return
         root = TreeNode(preorder[0])
         low = [i for i in preorder if i < preorder[0]]
         high = [i for i in preorder if i > preorder[0]]
-
+        
+        
         root.left = self.bstFromPreorder(low)
         root.right = self.bstFromPreorder(high)
 
         return root
+
 
