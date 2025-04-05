0191bf7364aab93fa0649a36e7364177839c0dd5
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-04-05 09:31:39 -0700

    Time: 3 ms (3.87%), Space: 18 MB (15.99%) - LeetHub

--- 1005-univalued-binary-tree/1005-univalued-binary-tree.py
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.py
@@ -6,20 +6,18 @@
 #         self.right = right
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
-        unival =True
         prev = None
-        def dfs(node):
+        unival =True
+        def solve(node):
             nonlocal prev, unival
             if node is None:
-                return
-            dfs(node.left)
+                return 
+
+            solve(node.left)
             if prev is not None and prev != node.val:
                 unival = False
-            prev = node.val #updating prev to current node value if it is None
-            dfs(node.right)
-        
-    
+            prev = node.val
+            solve(node.right)
 
-        dfs(root)
+        solve(root)
         return unival
-        
