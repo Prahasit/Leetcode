6aef0862c7b1093fb16859b1dbe0da4e7445f264
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-04-05 08:17:30 -0700

    Time: 124 ms (6.19%), Space: 45.9 MB (13.12%) - LeetHub

--- 1731-even-odd-tree/1731-even-odd-tree.py
+++ 1731-even-odd-tree/1731-even-odd-tree.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        if root.val % 2 == 0:
+        if root.val % 2  == 0:
             return False
         q = deque()
         q.append((root, 0))
         result = []
+
         while q:
             l = []
             for _ in range(len(q)):
                 node, level = q.popleft()
-
                 if level % 2 == 0:
                     if node.val % 2 == 1:
                         if len(l) == 0 or node.val > l[-1]:
@@ -26,21 +26,23 @@ class Solution:
                         return False
 
                 else:
-                    if node.val %2 ==0:
-                        if len(l) == 0 or node.val < l[-1] and node.val % 2== 0:
+                    if node.val % 2 == 0:
+                        if len(l) == 0 or node.val < l[-1]:
                             l.append(node.val)
                         else:
                             return False
                     else:
                         return False
+
+                
                 if node.left:
                     q.append((node.left, level + 1))
                 if node.right:
                     q.append((node.right, level + 1))
-                
+            print(l)   
             result.append(l)
-
         return True
-                         
+
+
 
         
