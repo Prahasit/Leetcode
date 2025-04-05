ceb4d87be3a374ec4f00523fcdf7795491a9610e
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-04-04 22:50:32 -0700

    Time: 3 ms (80.93%), Space: 20.4 MB (25.76%) - LeetHub

--- 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.py
+++ 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.py
@@ -6,28 +6,26 @@
 #         self.right = right
 class Solution:
     def findFrequentTreeSum(self, root: Optional[TreeNode]) -> List[int]:
-        hashmap = defaultdict(int)
-        result = []
+        res = []
         if root is None:
-            return []
-        def dfs(node):
-
+            return res
+        def solve(node):
             if node is None:
                 return 0
-            left_sum = dfs(node.left)
-            right_sum = dfs(node.right)
 
-            cur_sum = left_sum + right_sum + node.val # the value of node is leftsubree + right subtree + its value
-            hashmap[cur_sum] += 1
-            return cur_sum
-            
-
-        dfs(root)
-        max_freq = max(hashmap.values())
+            left = solve(node.left)
+            right = solve(node.right)
+            value = node.val + left + right
+            freq[value] += 1
+            return value
         
-        for key, val in hashmap.items():
-            if val == max_freq:
-                result.append(key)
-        return result
+        freq = defaultdict(int)
+        solve(root)
 
+        max_freq = max(freq.values())
+        for key, val in freq.items():
+            if val == max_freq:
+                res.append(key)
+        
+        return res
 
