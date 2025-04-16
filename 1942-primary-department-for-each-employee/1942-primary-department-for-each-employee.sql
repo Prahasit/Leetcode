c438b5bbe7f167bf38a0bad5a04eb53ac59709c8
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-04-16 17:49:01 -0400

    Time: 618 ms (65.37%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-select employee_id, department_id from Employee where primary_flag = 'Y' or employee_id in (select employee_id from Employee group by employee_id having count(department_id) = 1)
+select employee_id, department_id from Employee where primary_flag = 'Y'
+Union
+Select employee_id, department_id from Employee group by employee_id having count(department_id) = 1
