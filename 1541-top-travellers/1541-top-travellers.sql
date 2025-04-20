d952229fa59b641301be778763e6b26830c24592
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-04-19 22:25:59 -0400

    Time: 809 ms (89.05%), Space: 0B (100%) - LeetHub

---
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+Select u.name, sum(case when u.id = r.user_id then r.distance else 0 end)  as travelled_distance from Users u left join Rides r on u.id = r.user_id group by r.user_id order by travelled_distance desc, u.name
