4eb05501806dd575becea3d3b40791bcae3dc722
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-12-20 08:44:22 -0800

    Time: 23 ms (19.09%), Space: 21.8 MB (36.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,10 +3,9 @@ class Solution:
         res = defaultdict(list)
 
         for s in strs:
-            chars = list(s)
-            chars.sort()
-            key = ''.join(chars)
-
-            res[key].append(s)
-    
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        
         return list(res.values())
