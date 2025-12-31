d23e4e0433d6afdea1c561916982e55748557453
Prahasit <78253914+Prahasit@users.noreply.github.com>
2025-12-30 18:42:04 -0800

    Time: 4 ms (9.77%), Space: 17.6 MB (87.55%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[- 1 for _ in range(n)] for _ in range(m)]
+        def dfs(i, j):
+            if obstacleGrid[i][j] == 1:
+                return 0
+            if i == 0 and j == 0:
+                return 1
+            if i < 0 or j < 0:
+                return 0
+            if dp[i][j] != -1:
+                return dp[i][j]
+            up = dfs(i - 1, j)
+            left = dfs(i, j - 1)
+
+            dp[i][j] = up + left
+            return dp[i][j]
+
+        return dfs(m - 1, n - 1)
