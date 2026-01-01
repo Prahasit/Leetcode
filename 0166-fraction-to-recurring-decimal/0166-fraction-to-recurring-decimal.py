311d3782b239aab7811b7e253e5a64a6fe77076d
Prahasit <78253914+Prahasit@users.noreply.github.com>
2026-01-01 07:59:19 -0800

    Time: 0 ms (100%), Space: 17.8 MB (74.98%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
@@ -1,36 +1,35 @@
 class Solution:
     def fractionToDecimal(self, numerator: int, denominator: int) -> str:
-        if numerator == 0:
-            return "0"
 
-        #determine the sum
         res = []
+        if numerator  == 0:
+            return '0'
+
         if (numerator < 0) != (denominator < 0):
             res.append('-')
-        #after sign now work with absolutie values
+
         num, den = abs(numerator), abs(denominator)
 
-        # interger part
-        res.append(str(num//den))
+        res.append(str(num // den))
         rem = num % den
 
-        # if no remainder then return
         if rem == 0:
             return "".join(res)
 
-        #if there is remainder 
         res.append('.')
         rem_map = {}
-
-        while rem != 0:
-            if rem in rem_map: # i.e cycle is repeating
+        while rem !=0:
+            if rem in rem_map:
                 pos = rem_map[rem]
                 res.insert(pos, '(')
                 res.append(')')
-                return  "".join(res)
+                return "".join(res)
 
             rem_map[rem] = len(res)
             rem *= 10
-            res.append(str(rem // den))
+            res.append(str(rem//den))
             rem %= den
+
         return "".join(res)
+
+    
